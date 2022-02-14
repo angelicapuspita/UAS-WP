c690884b874ae0e27882fcc097e898422ecb385e
Yohanes Wahyu Hardi Pratama <yohanes_wahyu@hotmail.com>
2022-02-15 06:49:37 +0700

    first commit

---
+++ resources/views/layouts/detail.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Amazing E-Book</title>
+        <link rel="icon" href="assets/images/items/1.jpg" type="image/x-icon"/>
+        
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+     
+        <link href="/css/bootstrap.css" rel="stylesheet">
+        <link href="/css/ui.css" rel="stylesheet">
+        <link href="/css/responsive.css" rel="stylesheet">
+        
+        <link href="/css/all.min.css" rel="stylesheet">
+        <script src="/js/jquery.min.js" type="text/javascript"></script>
+        <script src="/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+                
+    </head>
+
+<body>
+<section class="header-main border-bottom">
+  <div class="container">
+<div class="row align-items-center">
+
+  <div class="col-lg-2 col-6">
+    <a href="#" class="brand-wrap">
+      Amazing E-Book
+    </a>
+  </div>
+
+  <div class="col-lg-6 col-12 col-sm-12">
+    <form action="#" class="search">
+      <div class="input-group w-100">
+          <input type="text" class="form-control" placeholder="Search">
+          <div class="input-group-append">
+            <button class="btn btn-primary" type="submit">
+              <i class="fa fa-search"></i>
+            </button>
+          </div>
+        </div>
+    </form> 
+  </div> 
+  <div class="col-lg-4 col-sm-6 col-12">
+    <div class="widgets-wrap float-md-right">
+          <div>  
+            <a href="#"> Log Out</a>
+          </div>
+        </div>
+      </div>
+    </div> 
+  </div>
+</div> 
+  </div>
+</section>
+
+<nav class="navbar navbar-main navbar-expand-lg navbar-light border-bottom">
+  <div class="container">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav" aria-controls="main_nav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="main_nav">
+      <ul class="navbar-nav">
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cart</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Profile</a>
+        </li>
+      </ul>
+    </div> 
+  </div> 
+</nav>
+
+<h4>E-Book Detail</h4>
+<span>Title: </span>
+<span>Author:</span>
+<span>Description: </span>
+
+<script>src="https://code.jquery.com/jquery-3.2.1.slim.min.js"</script>
+        <script>src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"</script>
+</body>
+
+<footer>
+    <p>
+      &copy; Amazing E-Book 2022
+    </p>
+</footer>
+
+</html>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Amazing E-Book</title>
+        <link rel="icon" href="assets/images/items/1.jpg" type="image/x-icon"/>
+        
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+     
+        <link href="/css/bootstrap.css" rel="stylesheet">
+        <link href="/css/ui.css" rel="stylesheet">
+        <link href="/css/responsive.css" rel="stylesheet">
+        
+        <link href="/css/all.min.css" rel="stylesheet">
+        <script src="/js/jquery.min.js" type="text/javascript"></script>
+        <script src="/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+                
+    </head>
+
+<body>
+<section class="header-main border-bottom">
+  <div class="container">
+<div class="row align-items-center">
+
+  <div class="col-lg-2 col-6">
+    <a href="#" class="brand-wrap">
+      Amazing E-Book
+    </a>
+  </div>
+
+  <div class="col-lg-6 col-12 col-sm-12">
+    <form action="#" class="search">
+      <div class="input-group w-100">
+          <input type="text" class="form-control" placeholder="Search">
+          <div class="input-group-append">
+            <button class="btn btn-primary" type="submit">
+              <i class="fa fa-search"></i>
+            </button>
+          </div>
+        </div>
+    </form> 
+  </div> 
+  <div class="col-lg-4 col-sm-6 col-12">
+    <div class="widgets-wrap float-md-right">
+          <div> 
+            <a href="#">Sign Up</a> |  
+            <a href="#"> Log In</a>
+          </div>
+        </div>
+      </div>
+    </div> 
+  </div>
+</div> 
+  </div>
+</section>
+
+<section>
+<div class="rent">
+  <h3>Find and Rent Your E-Book Here!</h3>
+</div>
+</div>
+</section>
+
+<script>src="https://code.jquery.com/jquery-3.2.1.slim.min.js"</script>
+        <script>src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"</script>
+</body>
+
+<footer>
+    <p>
+      &copy; Amazing E-Book 2022
+    </p>
+</footer>
+
+</html>
